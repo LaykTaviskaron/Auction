6d0f64590e837c0511b03fef3bfc3672779f2cdf
Megatron <Megatron@HELLRIDER>
2017-05-21 23:12:51 +0300

    Fixed styles and validation for date

--- WebSite/Scripts/createItem.js
+++ WebSite/Scripts/createItem.js
@@ -1,56 +1,59 @@
-﻿$(document).ready(function () {
-        $(function () {
-            $('#datetimepicker').datetimepicker();
+﻿$(document)
+    .ready(function() {
+        $(function() {
+            $('#datetimepicker').datetimepicker({
+                format: 'DD/MM/YYYY HH:mm'
+            });
         });
     });
 
-    function getFeatures() {
-        var id = $("#categoryId").val();
-        $.ajax({
-            type: "GET",
-            url: '/Items/GetFeatures',
-            data: { categoryId: id },
-            success: function (viewHTML) {
-                $(".target").html(viewHTML);
-            },
-            error: function (errorData) { console.log(errorData); }
-        });
-    }
+function getFeatures() {
+    var id = $("#categoryId").val();
+    $.ajax({
+        type: "GET",
+        url: '/Items/GetFeatures',
+        data: { categoryId: id },
+        success: function (viewHTML) {
+            $(".target").html(viewHTML);
+        },
+        error: function (errorData) { console.log(errorData); }
+    });
+}
 
-    function getImage() {
-        $.ajax({
-            type: "GET",
-            url: '/Items/GetImage',
-            success: function (data) {
-                $("#item-image").attr("src", data);
-            },
-            error: function (errorData) { console.log(errorData); }
-        });
-    }
+function getImage() {
+    $.ajax({
+        type: "GET",
+        url: '/Items/GetImage',
+        success: function (data) {
+            $("#item-image").attr("src", data);
+        },
+        error: function (errorData) { console.log(errorData); }
+    });
+}
 
-    function uploadImage() {
-        var input = document.getElementById('file-btn'),
-            data = new FormData();
-        data.append("ItemImage", input.files[0]);
-        $.ajax({
-            type: "POST",
-            url: '/Items/ImageUpload',
-            data: data,
-            dataType: 'json',
-            contentType: false,
-            processData: false,
-            allways: getImage
-        }).always(getImage);
-    }
+function uploadImage() {
+    var input = document.getElementById('file-btn'),
+        data = new FormData();
+    data.append("ItemImage", input.files[0]);
+    $.ajax({
+        type: "POST",
+        url: '/Items/ImageUpload',
+        data: data,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        allways: getImage
+    }).always(getImage);
+}
 
-    function setSelectedFeature(featureId) {
-        var selectedValue = $("#feature-" + featureId).val();
-        $.ajax({
-            type: "POST",
-            url: '/Items/SetFeature',
-            data: { featureId: featureId, slectedValue: selectedValue }
-        });
-    }
+function setSelectedFeature(featureId) {
+    var selectedValue = $("#feature-" + featureId).val();
+    $.ajax({
+        type: "POST",
+        url: '/Items/SetFeature',
+        data: { featureId: featureId, slectedValue: selectedValue }
+    });
+}
 
-    getImage();
-    getFeatures();
+getImage();
+getFeatures();
