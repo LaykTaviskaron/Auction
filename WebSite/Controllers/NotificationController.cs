133ee2ba985810f23ecc629af7700c47ca2cc609
Artem Sokolov <artem.sokolov@sigma.software>
2017-05-22 22:05:53 +0300

    Added notification controller

---
+++ WebSite/Controllers/NotificationController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using WebGrease.Css.Extensions;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class NotificationController : BaseController
+    {
+        //GET: Notifications
+        public ActionResult Index()
+        {
+            var currentUserId = User.Identity.GetUserId();
+            var currentUser = this.DbContext.Accounts.Where(x => x.Email == "artem.sokolov@sigma.software").FirstOrDefault();
+
+            this.DbContext.Notifications.Add(new Notification
+            {
+                Account = currentUser,
+                Id = Guid.NewGuid(),
+                ReceiverId = currentUser?.Id,
+                Message = "welcome to your account"
+            });
+            this.DbContext.SaveChanges();
+
+            ViewBag.Notifications = this.DbContext.Notifications.Select(x => new NotificationViewModel
+            {
+                Id = x.Id,
+                Message = x.Message
+            }).ToList();
+
+            return View(ViewBag.Notifications);
+        }
+
+        //GET: Notifications/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notification notification = this.DbContext.Notifications.Find(id);
+            if (notification == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notification);
+        }
+
+        //GET: Notifications/Create
+        public ActionResult Create()
+        {
+            ViewBag.BuyerId = new SelectList(this.DbContext.Accounts, "Id", "FirstName");
+            ViewBag.SellerId = new SelectList(this.DbContext.Accounts, "Id", "FirstName");
+            ViewBag.HighestBetId = new SelectList(this.DbContext.Bets, "Id", "Id");
+            ViewBag.CategoryId = new SelectList(this.DbContext.Categories, "Id", "Name");
+            ViewBag.FeatureId = new SelectList(this.DbContext.Specifications, "Id", "");
+
+            return View();
+        }
+
+        //GET: Notifications/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notification notification = this.DbContext.Notifications.Find(id);
+            if (notification == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notification);
+        }
+
+        //POST: Notifications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Notification notification = this.DbContext.Notifications.Find(id);
+            this.DbContext.Notifications.Remove(notification);
+            this.DbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.DbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebSite/Models/NotificationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebSite.Models
+{
+    public class NotificationViewModel
+    {
+        public System.Guid Id { get; set; }
+        public string Message { get; set; }
+        public Guid? ItemId { get; set; }
+        public Guid? ReceiverId { get; set; }
+        public byte? NotificationTypeId { get; set; }
+
+        public virtual Account Account { get; set; }
+        public virtual NotificcationType NotificcationType { get; set; }
+        public virtual Item Item { get; set; }
+    }
+}
